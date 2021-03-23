9568f0739f1b3e1b630b3d4906c2ac235063bf03
sweetylenka <sweetylenka2013@gmail.com>
2021-03-23 21:48:39 +0530

    As a fan of geometry, I want to check equality of two lines based on the end points, So that I know when two lines are the equal. - Using Java equals method to check equality of 2 Lengths is preferable.

--- LineComparision.java
+++ LineComparision.java
@@ -1,21 +1,36 @@
 import java.util.Scanner;
 
+
 public class LineComparision {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	public static void main(String args[]) {
+	
+		// variables
+		double x1, y1, x2, y2; 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter co-ordinate x1 : ");
-		double x1=sc.nextDouble();
-		System.out.println("Enter co-ordinate y1 : ");
-		double y1=sc.nextDouble();
-		System.out.println("Enter co-ordinate x2 : ");
-		double x2=sc.nextDouble();
-		System.out.println("Enter co-ordinate y2 : ");
-		double y2=sc.nextDouble();
-
-		double Lenght = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-		System.out.println("Length of Line : "+Lenght) ;
+		System.out.println("Enter co-ordinates of First Line : ");
+		x1=sc.nextDouble();
+		y1=sc.nextDouble();
+		x2=sc.nextDouble();
+		y2=sc.nextDouble();
+		
+		double Lenght1 = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+		System.out.println("Length of Line1 : "+Lenght1) ;
+		
+		System.out.println("Enter co-ordinates of Second Line : ");
+		x1=sc.nextDouble();
+		y1=sc.nextDouble();
+		x2=sc.nextDouble();
+		y2=sc.nextDouble();
+		
+		double Lenght2 = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+		System.out.println("Length of Line2 : "+Lenght2) ;
+		
+		if(Double.compare(Lenght1,Lenght2)==0)
+		
+			System.out.println("Length of Line 1 = Lenght of Line 2");
+		else
+			System.out.println("Length of Line 1 != Lenght of Line 2");
 	}
+		
 }
-
